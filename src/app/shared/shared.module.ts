c1a508a6bdc671a633fa8c2df2ce230fdf6b4dc8
Bamgbala Shuaib Adeyemi <adeyemi.bamgbala@gmail.com>
2021-08-06 16:49:45 +0100

    Add data filter to table

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -13,11 +13,15 @@ import { MatMenuModule } from '@angular/material/menu';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [CardComponent, TableComponent],
   imports: [
     CommonModule,
+    FormsModule,
     MatToolbarModule,
     MatButtonModule,
     MatSidenavModule,
@@ -29,6 +33,8 @@ import { MatSortModule } from '@angular/material/sort';
     MatTableModule,
     MatPaginatorModule,
     MatSortModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   exports: [
     CardComponent,
@@ -44,6 +50,7 @@ import { MatSortModule } from '@angular/material/sort';
     MatTableModule,
     MatPaginatorModule,
     MatSortModule,
+    MatFormFieldModule,
   ],
 })
 export class SharedModule {}
